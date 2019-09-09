60a7f55ba514a69c362af4d71b2e927d55cf0642
mikitty42 <mikiaaamiki@yahoo.co.jp>
2019-09-09 23:59:36 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'dengonban/index'
+  get 'helo/index'
+  get 'helo', to: 'helo#index'
+  get 'helo/other'
+  post 'helo/index'
+  post 'helo', to: 'helo#index'
+  get 'dengonban', to: 'dengonban#index'
+  get 'dengonban/index'
+  post 'dengonban', to: 'dengonban#index'
+  post 'dengonban/index'
+end
